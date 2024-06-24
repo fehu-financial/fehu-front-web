bfa24527653d4eb305d2bef46f4a96948dae129a
Igor Souza <igorsouza.96@hotmail.com>
2024-06-24 13:35:27 -0300

    ci: fix github workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { SideNav } from "@/components/layout/sidenav";
