42472616bba9a73c32cc653120b5dd7319b39ba5
Igor Souza <igorsouza.96@hotmail.com>
2024-08-15 23:43:01 -0300

    feat(ui): add main layout with header, nav, and main
    Introduce main layout with header, navigation (sidebar menu), and main content.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
-import "./globals.css";
+import { Header } from "@/components/layout/header";
+import { Main } from "@/components/layout/main";
+import { Navigation } from "@/components/layout/navigation";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
 	title: "Fehu Finances",
 	description: "Gerencie investimentos, despesas e tudo mais.",
 };
 
-export default function RootLayout({
-	children,
-}: {
+type RootLayoutProps = {
 	children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" className="dark">
+			<body
+				className={cn(
+					"grid grid-cols-[80px,1fr] grid-rows-[80px,1fr] gap-2 h-screen antialiased",
+					inter.className,
+				)}
+			>
+				<Navigation />
+				<Header />
+				<Main>{children}</Main>
+			</body>
 		</html>
 	);
 }
