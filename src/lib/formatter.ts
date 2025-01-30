4041f5ec3a491f73dab8fedf405dd18989f27c9e
Igor Souza <igorsouza.96@hotmail.com>
2025-01-30 16:39:09 -0300

    feat(ui): add data table components and formatter functions

--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -1,27 +1,31 @@
-export default class Formatter {
-	static currency(value: number): string {
-		return new Intl.NumberFormat("pt-BR", {
-			style: "currency",
-			currency: "BRL",
-		}).format(value);
-	}
-
-	datetime(value: number | string | Date): string {
-		const date = new Date(value);
-		const options: Intl.DateTimeFormatOptions = {
-			timeStyle: "medium",
-			dateStyle: "short",
-			hour12: false,
-		};
-		return Intl.DateTimeFormat("pt-BR", options).format(date);
-	}
-
-	date(value: number | string | Date): string {
-		const date = new Date(value);
-		const options: Intl.DateTimeFormatOptions = {
-			dateStyle: "short",
-			hour12: false,
-		};
-		return Intl.DateTimeFormat("pt-BR", options).format(date);
-	}
+export function currency(value: number): string {
+	return new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	}).format(value);
 }
+
+export function datetime(value: number | string | Date): string {
+	const date = new Date(value);
+	const options: Intl.DateTimeFormatOptions = {
+		timeStyle: "medium",
+		dateStyle: "short",
+		hour12: false,
+	};
+	return Intl.DateTimeFormat("pt-BR", options).format(date);
+}
+
+export function date(value: number | string | Date): string {
+	const date = new Date(value);
+	const options: Intl.DateTimeFormatOptions = {
+		dateStyle: "short",
+		hour12: false,
+	};
+	return Intl.DateTimeFormat("pt-BR", options).format(date);
+}
+
+export const Formatter = {
+	date,
+	datetime,
+	currency,
+};
