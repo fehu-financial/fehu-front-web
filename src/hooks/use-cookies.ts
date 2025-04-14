4e81e455bf10b254e9edbd0d081f757cb1f14cb4
Igor Souza <igorsouza.96@hotmail.com>
2025-04-14 09:53:44 -0300

    fix: lint

--- src/hooks/use-cookies.ts
+++ src/hooks/use-cookies.ts
@@ -2,47 +2,47 @@ import Cookies from "js-cookie";
 import { useCallback, useState } from "react";
 
 type UseCookieReturn<T> = {
-  value: T;
-  setCookie: (newValue: T, options?: Cookies.CookieAttributes) => void;
-  removeCookie: () => void;
+	value: T;
+	setCookie: (newValue: T, options?: Cookies.CookieAttributes) => void;
+	removeCookie: () => void;
 };
 
 export default function useCookie<T>(
-  name: string,
-  defaultValue: T,
-  type?: "json" | "base64" | "string"
+	name: string,
+	defaultValue: T,
+	type?: "json" | "base64" | "string",
 ): UseCookieReturn<T> {
-  const [value, setValue] = useState<T>(() => {
-    const cookie = Cookies.get(name);
-    console.log("cookie", cookie);
-    const parsedCookie: T =
-      type === "base64"
-        ? atob(cookie || "")
-        : type === "json"
-        ? JSON.parse(cookie || "")
-        : cookie;
+	const [value, setValue] = useState<T>(() => {
+		const cookie = Cookies.get(name);
+		console.log("cookie", cookie);
+		const parsedCookie: T =
+			type === "base64"
+				? atob(cookie || "")
+				: type === "json"
+					? JSON.parse(cookie || "")
+					: cookie;
 
-    if (parsedCookie) return parsedCookie;
-    if (!defaultValue || defaultValue === null) return defaultValue;
+		if (parsedCookie) return parsedCookie;
+		if (!defaultValue || defaultValue === null) return defaultValue;
 
-    // const parsedDefaultValue = btoa(JSON.stringify(defaultValue));
-    // Cookies.set(name, parsedDefaultValue);
-    return defaultValue;
-  });
+		// const parsedDefaultValue = btoa(JSON.stringify(defaultValue));
+		// Cookies.set(name, parsedDefaultValue);
+		return defaultValue;
+	});
 
-  const setCookie = useCallback(
-    (newValue: T, options?: Cookies.CookieAttributes) => {
-      if (!newValue || newValue === null) return;
-      Cookies.set(name, btoa(JSON.stringify(newValue)), options);
-      setValue(newValue);
-    },
-    [name]
-  );
+	const setCookie = useCallback(
+		(newValue: T, options?: Cookies.CookieAttributes) => {
+			if (!newValue || newValue === null) return;
+			Cookies.set(name, btoa(JSON.stringify(newValue)), options);
+			setValue(newValue);
+		},
+		[name],
+	);
 
-  const removeCookie = useCallback(() => {
-    Cookies.remove(name);
-    setCookie(defaultValue);
-  }, [name, defaultValue, setCookie]);
+	const removeCookie = useCallback(() => {
+		Cookies.remove(name);
+		setCookie(defaultValue);
+	}, [name, defaultValue, setCookie]);
 
-  return { value, setCookie, removeCookie };
+	return { value, setCookie, removeCookie };
 }
